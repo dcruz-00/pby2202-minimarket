0956c7ea2e2daf144c14df18e145acb99edc8a2d
dcruz-00 <di.cruzv@duocuc.cl>
2026-05-23 21:43:53 -0400

    agregar @JsonIgnore al campo password

--- src/main/java/com/minimarket/entity/Usuario.java
+++ src/main/java/com/minimarket/entity/Usuario.java
@@ -2,6 +2,7 @@ package com.minimarket.entity;
 
 import jakarta.persistence.*;
 import java.util.Set;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Usuario {
@@ -12,6 +13,7 @@ public class Usuario {
     @Column(nullable = false, unique = true)
     private String username;
 
+    @JsonIgnore
     @Column(nullable = false)
     private String password;
 
