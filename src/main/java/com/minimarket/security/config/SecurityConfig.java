17440f0854c44aaf4018647983b37aa1a97f212e
dcruz-00 <di.cruzv@duocuc.cl>
2026-05-31 22:15:52 -0400

    Se añade JwtAuthFilter en SecurityConfig

--- src/main/java/com/minimarket/security/config/SecurityConfig.java
+++ src/main/java/com/minimarket/security/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.minimarket.security.config;
 
-import com.minimarket.security.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,10 +7,14 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.http.SessionCreationPolicy;
+
+import com.minimarket.security.filter.JwtAuthFilter;
+import com.minimarket.security.service.CustomUserDetailsService;
+
 
 @Configuration
 @EnableWebSecurity
@@ -19,30 +22,33 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 public class SecurityConfig {
 
     private final CustomUserDetailsService customUserDetailsService;
+    private final JwtAuthFilter jwtAuthFilter;
 
-    public SecurityConfig(CustomUserDetailsService customUserDetailsService) {
+    public SecurityConfig(CustomUserDetailsService customUserDetailsService, JwtAuthFilter jwtAuthFilter) {
         this.customUserDetailsService = customUserDetailsService;
+        this.jwtAuthFilter = jwtAuthFilter;
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // Deshabilita CSRF con la nueva sintaxis
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/public/**").permitAll() // Permitir acceso público
-                        .requestMatchers("/api/usuarios").hasRole("GERENTE") // Ordenado de acceso más a menos restrictivo.
-                        .requestMatchers("/api/carrito").hasRole("CLIENTE")
-                        .requestMatchers("/api/inventario", "/api/ventas", "/api/detalle-ventas").hasAnyRole("GERENTE", "EMPLEADO")
-                        .requestMatchers("/api/productos", "/api/categorias").hasAnyRole("GERENTE", "EMPLEADO", "CLIENTE")
-                        .anyRequest().authenticated()) 
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/api/auth/**").permitAll() // primero las rutas públicas
+                    .requestMatchers("/public/**").permitAll()
+                    .requestMatchers("/api/usuarios").hasRole("GERENTE")
+                    .requestMatchers("/api/carrito").hasRole("CLIENTE")
+                    .requestMatchers("/api/inventario", "/api/ventas", "/api/detalle-ventas").hasAnyRole("GERENTE", "EMPLEADO")
+                    .requestMatchers("/api/productos", "/api/categorias").hasAnyRole("GERENTE", "EMPLEADO", "CLIENTE")
+                    .anyRequest().authenticated())
                         /*
                         Se agregó sessionManagement para configurar la política de creación de sesiones. En este caso, se establece como STATELESS, 
                         lo que significa que no se crearán sesiones y cada solicitud 
                         se autenticarán de forma independiente. 
                         */
-                        .sessionManagement(httpSecuritySessionManagmentConfigurar -> httpSecuritySessionManagmentConfigurar
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));     
-                return http.build();
+            .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .addFilterBefore(jwtAuthFilter, org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
 
     @Bean
